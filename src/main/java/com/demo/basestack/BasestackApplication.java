73c1ec1d6d801d7f4ffe1e2bb475e294714b26f8
haithemelmetoui <elmetoui.haithem@gmail.com>
2024-11-19 19:37:39 +0100

    first commit

---
+++ src/main/java/com/demo/basestack/BasestackApplication.java
@@ -0,0 +1,13 @@
+package com.demo.basestack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasestackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasestackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/basestack/BasestackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.basestack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasestackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
